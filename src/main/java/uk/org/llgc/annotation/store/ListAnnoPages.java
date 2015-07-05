0598f34dab3178311692781e4c22ec4911f48a9e
glenrobson <gmr@nlw>
2015-07-05 01:31:02 +0100

    Adding a list all annoatations page
    After a request from Régis and Paul McCann I'm adding a list.html to allow you to see what annotations are stored in the Annotation store. Partical fix for #4.

---
+++ src/main/java/uk/org/llgc/annotation/store/ListAnnoPages.java
@@ -0,0 +1,73 @@
+package uk.org.llgc.annotation.store;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+import com.github.jsonldjava.utils.JsonUtils;
+
+import com.hp.hpl.jena.tdb.TDBFactory;
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.rdf.model.Model;
+
+import java.util.Map;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import uk.org.llgc.annotation.store.data.PageAnnoCount;
+
+public class ListAnnoPages extends HttpServlet {
+	protected AnnotationUtils _annotationUtils = null;
+	protected StoreAdapter _store = null;
+
+	public void init(final ServletConfig pConfig) throws ServletException {
+		super.init(pConfig);
+		Dataset tDataset = TDBFactory.createDataset(super.getServletContext().getInitParameter("data_dir"));
+		_annotationUtils = new AnnotationUtils(new File(super.getServletContext().getRealPath("contexts")));
+		_store = new StoreAdapter(tDataset);
+	}
+
+	public void doGet(final HttpServletRequest pReq, final HttpServletResponse pRes) throws IOException {
+		List<PageAnnoCount> tAnnotations = _store.listAnnoPages();
+		System.out.println(tAnnotations);
+
+		StringBuffer tContent = new StringBuffer();
+		for (PageAnnoCount tPage : tAnnotations) {
+			tContent.append("<li><a href=\"/annotation/search?uri=");
+			tContent.append(tPage.getPageId());
+			tContent.append("\">");
+			tContent.append(tPage.getPageId());
+			tContent.append("</a> ");
+			tContent.append("(");
+			tContent.append(tPage.getCount());
+			tContent.append(" annotations)");
+			tContent.append("</li>");
+		}
+
+		File tTemplate = new File(new File(super.getServletContext().getRealPath("templates")), "list.template");
+		BufferedReader tReader = null;
+		try {
+			tReader = new BufferedReader(new FileReader(tTemplate));
+			String tLine = null;
+			StringBuffer tHTML = new StringBuffer();
+			while ((tLine = tReader.readLine()) != null ) {
+				tHTML.append(tLine);
+			}
+
+			String tResult = tHTML.toString().replaceAll("##CONTENT##", tContent.toString());
+
+			pRes.setContentType("text/html");
+			pRes.getOutputStream().println(tResult);
+		} finally {
+			if (tReader != null) {
+				tReader.close();
+			}
+		}
+	}
+}
