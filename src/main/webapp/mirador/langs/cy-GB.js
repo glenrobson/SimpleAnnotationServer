11cbdc74333b306db51690d6d85e21067d253d11
Paul McCann <paul@sankesolutions.co.uk>
2015-11-06 15:04:23 +0000

    modifications made to build for translation of editor buttons see mirador book_of_rememberance for full changes

---
+++ src/main/webapp/mirador/langs/cy-GB.js
@@ -0,0 +1,16 @@
+tinymce.addI18n('cy_GB', {
+    "heading": "Pennawd",
+    "rank": "Rheng",
+    "name": "Enw",
+    "place": "Lle",
+    "unit": "Uned",
+    "ship": "Llong",
+    "medal": "Medal",
+    "Highlight heading":"Aroleuo pennawd",
+    "Highlight Rank":"Aroleuo rheng",
+    "Highlight Name":"Aroleuo enw",
+    "Highlight placename":"Aroleuo lle",
+    "Highlight regiment, batt, div, unit etc":"Aroleuo uned",
+    "Highlight ship name":"Aroleuo llong",
+    "Highlight medal":"Aroleuo medal"
+});
