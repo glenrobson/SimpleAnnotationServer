22b19c88a35df9a3a39e414936386a1575d4e1f8
glenrobson <gmr@nlw>
2016-07-07 03:01:17 +0100

    Adding exception if an annotation is added with a duplicate id

---
+++ src/main/java/uk/org/llgc/annotation/store/exceptions/IDConflictException.java
@@ -0,0 +1,11 @@
+package uk.org.llgc.annotation.store.exceptions;
+
+public class IDConflictException extends Exception {
+	public IDConflictException() {
+		super();
+	}
+
+	public IDConflictException(final String pMessage) {
+		super(pMessage);
+	}
+}
