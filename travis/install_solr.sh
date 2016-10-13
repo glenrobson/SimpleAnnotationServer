08907ded4018ee5b07aae47586d9a1b21e56d030
glenrobson <glen.robson@gmail.com>
2016-10-14 00:08:14 +0100

    Adding a download of SOLR script

---
+++ travis/install_solr.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+download() {
+	FILE="$2.tgz"
+	if [ -f $FILE ]; then
+		echo "File $FILE exists."
+		tar -zxf $FILE
+	else
+		echo "File $FILE does not exist. Downloading solr from $1..."
+		curl -O $1
+		tar -zxf $FILE
+	fi
+	echo "Downloaded!"
+}
+
+SOLR_VERSION="6.2.1"
+SOLR_DIR="solr-$SOLR_VERSION"
+SOLR_CORE="testannotations"
+download "http://archive.apache.org/dist/lucene/solr/$SOLR_VERSION/solr-$SOLR_VERSION" "solr-SOLR_VERSION"
+
+SOLR_CORE_HOME="$SOLR_DIR/server/solr/$SOLR_CORE"
+cp -r src/main/resources/solr $SOLR_CORE_HOME
+
+cd $SOLR_DIR
+./bin/solr start
