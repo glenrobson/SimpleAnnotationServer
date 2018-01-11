6bc923f052a226758b74a2c5d126870910fb5264
Glen Robson <glen.robson@gmail.com>
2018-01-11 00:06:21 +0000

    Running a full linux instance. Should probably split components into seperate docker files.

--- docker/latestMiradorExternal/runDocker.sh
+++ docker/latestMiradorExternal/runDocker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --rm -t sas:latest . && docker run -ti --cap-add SYS_ADMIN -v /sys/fs/cgroup:/sys/fs/cgroup:ro --rm --name sas -p 9001:80 sas:latest 
-
+docker build --rm -t sas:latest . && docker run --rm -ti --tmpfs /tmp --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 9002:80 -p 8983:8983 -p 9080:8080 --privileged --name sas sas:latest
+# docker run -ti --rm --name sas -p 9001:80 -e PORT=80 sas:latest 
 
