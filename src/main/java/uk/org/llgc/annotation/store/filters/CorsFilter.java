320189a9a673ab12a6eaaff5308ba56f8eabcfe1
Glen Robson <glen.robson@gmail.com>
2018-07-24 01:00:40 +0100

    Sending CORS headers even if there is an error.

--- src/main/java/uk/org/llgc/annotation/store/filters/CorsFilter.java
+++ src/main/java/uk/org/llgc/annotation/store/filters/CorsFilter.java
@@ -16,11 +16,12 @@ public class CorsFilter implements Filter {
 	}
 
 	public void doFilter(final ServletRequest pReq, final ServletResponse pRes, final FilterChain pChain) throws IOException, ServletException {
-		pChain.doFilter(pReq, pRes);
+        // Never seems to returng from doFilter so setting CORS headers early. 
+        ((HttpServletResponse)pRes).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse)pRes).addHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+        ((HttpServletResponse)pRes).addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
 
-		((HttpServletResponse)pRes).addHeader("Access-Control-Allow-Origin", "*");
-		((HttpServletResponse)pRes).addHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
-		((HttpServletResponse)pRes).addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+        pChain.doFilter(pReq, pRes);
 	}
 
 	public void destroy() {
