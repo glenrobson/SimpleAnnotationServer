ec176a03cdb20ac8b5cab6bd910d81072702b4d7
Glen Robson <glen.robson@gmail.com>
2021-01-05 17:11:25 +0000

    Adding Mirador3

--- src/main/node/src/index.js
+++ src/main/node/src/index.js
@@ -1,14 +1,26 @@
 import Mirador from 'mirador/dist/es/src/index';
+import annotationPlugins from 'mirador-annotations/es/';
+import SimpleAnnotationServerV2Adapter from 'mirador-annotations/es/SimpleAnnotationServerV2Adapter';
+import LocalStorageAdapter from 'mirador-annotations/es/LocalStorageAdapter';
 
 const config = {
   id: 'mirador-viewer',
+  annotation: {
+   // adapter: (canvasId) => new SimpleAnnotationServerV2Adapter(canvasId, 'annotation'),
+     adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
+  },
+  window: {
+    defaultSideBarPanel: 'annotations',
+    sideBarOpenByDefault: true,
+  },
   windows: [
     {
-      manifestId: 'https://purl.stanford.edu/fr426cg9537/iiif/manifest',
+      manifestId: 'https://damsssl.llgc.org.uk/iiif/2.0/1132285/manifest.json',
     }
   ],
 };
 
-const plugins = [];
+const plugins = [ ...annotationPlugins ]
+//[...annotationPlugins];
 
 Mirador.viewer(config, plugins);
