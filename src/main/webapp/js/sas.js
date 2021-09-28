9273a4666b77c86d2baae01aea482d76dfc88fce
Glen Robson <glen.robson@gmail.com>
2021-09-28 20:31:38 +0100

    Adding enter click submit

--- src/main/webapp/js/sas.js
+++ src/main/webapp/js/sas.js
@@ -110,3 +110,18 @@ function relativeToAbsolute(el) {
 
 }
 
+function setupEnter(inputId, buttonId) {
+    // Get the input field
+    var input = document.getElementById(inputId);
+
+    // Execute a function when the user releases a key on the keyboard
+    input.addEventListener("keyup", function(event) {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById(buttonId).click();
+      }
+    }); 
+}
